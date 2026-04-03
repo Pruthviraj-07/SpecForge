b1f4d1321a2256de99b374653a93b51a0b357376
Avi@2000005 <chateavinash123@gmail.com>
2026-04-03 13:17:06 +0530

    Pushing backend folder structure

---
+++ backend/app.js
@@ -0,0 +1,4 @@
+PORT=5000
+MONGO_URI = "mongodb+srv://Avinash:Avi2005@avinashcluster.dhskht7.mongodb.net/usersDB/ignisia"
+GEMINI_API_KEY=your_gemini_api_key
+GOOGLE_MAPS_API_KEY=your_google_maps_api_key
